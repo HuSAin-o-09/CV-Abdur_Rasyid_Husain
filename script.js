85264963cb6f0c34fb6d2277d61910dbe73ed74d
husainN <abdurrasyidhusain6@gmail.com>
2026-09-23 22:38:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ function toggletheme() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+
+    const btn = document.getElementById("btn-theme");
+    if (body.classList.contains("dark-mode")) {
+        btn.innerHTML = "Light Mode";
+    } else {
+        btn.innerHTML = "Dark mode";
+    }
+ }
